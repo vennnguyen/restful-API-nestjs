5310d8efbf3a4d6eed3ac85b13e3dc8313873877
atuan41005 <tuan100205@gmail.com>
2025-11-17 10:38:35 +0700

    create module users

---
+++ src/users/schemas/user.schema.ts
@@ -0,0 +1,35 @@
+
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema()
+export class User {
+  @Prop({required: true, unique: true})
+  email: string;
+
+  @Prop({required: true, unique: true})
+  password: number;
+
+  @Prop()
+  name: string;
+
+  @Prop()
+  phone: string;
+
+  @Prop()
+  age: number;
+
+  @Prop()
+  address: string;
+
+  @Prop()
+  createAt: Date;
+
+    @Prop()
+  updateAt: Date;
+
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
---
+++ src/users/users.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './schemas/user.schema';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+
+@Injectable()
+export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+  async create(email:string, password:string,name:string) {
+    let user = await this.userModel.create({email,password,name})
+    return 'This action adds a new user';
+  }
+
+  findAll() {
+    return `This action returns all users`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
